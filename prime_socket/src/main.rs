b2d83bb9c96dd97971b4ac72a7053d7d1d1e6f4b
matthieugras <35115728+matthieugras@users.noreply.github.com>
2018-05-21 06:30:06 +0200

    Add files via upload

--- prime_socket/src/main.rs
+++ prime_socket/src/main.rs
@@ -1,8 +1,9 @@
 extern crate tokio_uds;
 extern crate futures;
 extern crate tokio_core;
+extern crate tokio;
 
-use tokio_uds::{UnixListener, UnixStream};
+use tokio_uds::{UnixListener};
 use futures::{Stream, Future};
 use tokio_core::reactor::Core;
 use std::io::Read;
@@ -12,7 +13,7 @@ use std::process::Command;
 use std::time::Duration;
 use std::thread::sleep;
 use std::io::{Error, ErrorKind};
-use std::cell::{RefCell, RefMut};
+use tokio::io;
 
 enum GPUState {
     Nvidia,
@@ -22,7 +23,7 @@ enum GPUState {
 fn get_curr_state() -> Result<GPUState, Error> {
     let power_profile_path = "/etc/prime-discrete";
     let mut curr_str = String::new();
-    let fd = File::open(power_profile_path)?.read_to_string(&mut curr_str)?;
+    File::open(power_profile_path)?.read_to_string(&mut curr_str)?;
     eprintln!("{}", curr_str.trim());
     match curr_str.trim() {
         "off" => Ok(GPUState::Intel),
@@ -38,63 +39,76 @@ fn main() {
     let ctl = match UnixListener::bind(addr, &handle) {
         Ok(l) => l,
         Err(_) => {
-            fs::remove_file(addr).unwrap();
-            UnixListener::bind(addr, &handle).unwrap()
+            fs::remove_file(addr).expect("Could not remove existing sock file");
+            UnixListener::bind(addr, &handle).expect("Could not bind after removing existing socket")
         }
     };
 
     let tk = ctl.incoming()
-        .for_each(|(_, _)| {
+        .for_each(|(sock, _)| {
             let curr_state = get_curr_state()?;
-
-            let kill_x = Command::new("sh")
-                .arg("-c")
-                .arg("systemctl disable lightdm && systemctl stop lightdm")
-                .status()?;
-
-            if !kill_x.success() {
-                return Err(Error::new(ErrorKind::Other, "Could not stop lightdm"));
-            }
-
-            match curr_state {
-                GPUState::Intel => {
-                    sleep(Duration::from_millis(500));
-                    let load_mods = Command::new("sh")
+            let cont_other = io::write_all(sock, "y")
+                .and_then(move |_| {
+                    let kill_x = Command::new("sh")
                         .arg("-c")
-                        .arg("modprobe nvidia")
+                        .arg("systemctl disable lightdm && systemctl stop lightdm")
                         .status()?;
 
-                    if !load_mods.success() {
-                        return Err(Error::new(ErrorKind::Other, "Could not load nvidia module"));
+                    if !kill_x.success() {
+                        return Err(Error::new(ErrorKind::Other, "Could not stop lightdm"));
                     }
-                },
-                GPUState::Nvidia => {
+
+                    match curr_state {
+                        GPUState::Intel => {
+                            sleep(Duration::from_millis(500));
+                            let load_mods = Command::new("sh")
+                                .arg("-c")
+                                .arg("modprobe nvidia")
+                                .status()?;
+
+                            if !load_mods.success() {
+                                return Err(Error::new(ErrorKind::Other, "Could not load nvidia module"));
+                            }
+                        },
+                        GPUState::Nvidia => {
+                            sleep(Duration::from_millis(500));
+                            let unload_mods = Command::new("sh")
+                                .arg("-c")
+                                .arg("rmmod nvidia-drm && rmmod nvidia-uvm && rmmod nvidia-modeset && rmmod nvidia && modprobe bbswitch && echo OFF > /proc/acpi/bbswitch")
+                                .status()?;
+
+                            if !unload_mods.success() {
+                                return Err(Error::new(ErrorKind::Other, "Could not unload nvidia modules"));
+                            }
+                        }
+                    }
+
                     sleep(Duration::from_millis(500));
-                    let unload_mods = Command::new("sh")
+                    let restart_x = Command::new("sh")
                         .arg("-c")
-                        .arg("rmmod nvidia-drm && rmmod nvidia-uvm && rmmod nvidia-modeset && rmmod nvidia && modprobe bbswitch && echo OFF > /proc/acpi/bbswitch")
+                        .arg("dpkg-reconfigure lightdm && systemctl start lightdm")
                         .status()?;
 
-                    if !unload_mods.success() {
-                        return Err(Error::new(ErrorKind::Other, "Could not unload nvidia modules"));
+                    if !restart_x.success() {
+                        return Err(Error::new(ErrorKind::Other, "Could not restart lightdm"));
                     }
-                }
-            }
 
-            sleep(Duration::from_millis(500));
-            let restart_x = Command::new("sh")
-                .arg("-c")
-                .arg("dpkg-reconfigure lightdm && systemctl start lightdm")
-                .status()?;
+                    Ok(())
+                })
+                .map_err(|e| {
+                    println!("Error = {}", e);
+                });
 
-            if !restart_x.success() {
-                return Err(Error::new(ErrorKind::Other, "Could not restart lightdm"));
-            }
+            handle.spawn(cont_other);
+
             Ok(())
         })
         .map_err(|e| {
             eprintln!("Error = {}", e);
         });
 
-    core.run(tk);
+    match core.run(tk) {
+        Err(_) => std::process::exit(1),
+        _ => std::process::exit(0)
+    }
 }
